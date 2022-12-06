ef959521b914e9899a75a211172052452a3e2298
Liza <Lizamerz@mail.ru>
2022-12-06 20:22:38 +0300

    + Beaver for Zoo

---
+++ src/zoo/experimental/Beaver.java
@@ -0,0 +1,39 @@
+package zoo.experimental;
+
+public class Beaver {
+    String name;
+    String breed;
+    static final String [] allColors = { "черный", "желтый" , "коричневый", "белый", "полосатый" }; //общий для всех бобров и не изменный
+    String color;
+
+    public Beaver(String name, String breed, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.color = color;
+
+    }
+
+    public void action() {
+        System.out.println(name + "маши хвостом");
+    }
+
+    public void getVoice() {
+        System.out.println(name);
+        say();
+    }
+
+    private void say() {
+        System.out.println("Где мое бревно?");
+    }
+    public void buildPlotina  ()
+    {
+        if(color.equals("черный") || color.equals("коричневый")||color.equals("оранжевый"))
+            System.out.println(color + " " + name +"может построить плотину");
+        else
+            System.out.println(name + " не может строить плотины "+breed);
+    }
+}
+
+
+
+
