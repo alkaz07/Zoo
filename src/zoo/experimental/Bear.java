6199ba48c408ecf2337ee8fa51759db4fc30492d
Igor240693 <igor_sumtsov@mail.ru>
2022-12-06 20:53:06 +0300

    + Bear for zoo

---
+++ src/zoo/experimental/Bear.java
@@ -0,0 +1,33 @@
+package zoo.experimental;
+
+public class Bear {
+    String color;
+    String breed;
+    int age;
+    public Bear(String color, String breed, int age){
+        this.color = color;
+        this.breed = breed;
+        if(age<0)
+            this.age = 0;
+        else
+            this.age=age;
+    }
+    public void getVoice(){
+        System.out.println(color);
+        growl();
+}
+
+    private void growl()
+    {
+        System.out.println("rrrrrrrr");
+    }
+
+    public void catchTheFish()
+    {
+        if(breed.equals("серый") || breed.equals("бурый")||breed.equals("черный")  )
+            System.out.println(color + " живёт в лесу");
+        if (breed.equals("белый"))
+            System.out.println(color + " живёт во льдах"+breed);
+    }
+}
+
