b274e6f2e1da86e8cf1387407bb05949a3bcffab
alkaz07 <alkaz07@yandex.ru>
2022-12-06 19:28:09 +0300

    + Dog + package zoo.experimental

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -3,6 +3,8 @@ package zoo;
 public class Zoo {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
+
+
---
+++ src/zoo/experimental/Dog.java
@@ -0,0 +1,31 @@
+package zoo.experimental;
+
+public class Dog{
+    String name;
+    String breed;
+    int age;
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        if(age<0)
+            this.age = 0;
+        else
+            this.age=age;
+    }
+    public void getVoice(){
+        System.out.println(name);
+        bark();
+    }
+    private void bark()
+    {
+        System.out.println("гав-гав-гав");
+    }
+
+    public void guideSheeps()
+    {
+        if(breed.equals("овчарка") || breed.equals("лайка")||breed.equals("дворн€га"))
+            System.out.println(name+" может пасти овец, где стадо?");
+        else
+            System.out.println(name + " не может пасти овец, потом у что "+breed);
+    }
+}
