72d235370aca5e9f8294a524325117c883e1f5fd
Vasya <vasya.454@gmail.com>
2022-12-12 10:39:42 +0300

    Сделал Woodpecker fromString() и move() поправил конструкторы, переименовал luck.

--- src/zoo/experimental/birds/Woodpecker.java
+++ src/zoo/experimental/birds/Woodpecker.java
@@ -1,5 +1,7 @@
 package zoo.experimental.birds;
 
+import java.util.Scanner;
+
 public class Woodpecker extends Bird {
     private double luck = 0.05;
     static final double MIN_LUCK = 0.001;
@@ -11,18 +13,12 @@ public class Woodpecker extends Bird {
         this.flyable = true;
     }
 
-
     public Woodpecker(String name, int age, double weight) {
         super("ƒ€тел", name, age, weight,true);
         System.out.println("запущен конструктор Woodpecker с параметрами");
-/*        this.species = "ƒ€тел";
-        this.flyable = true;
-        this.name = name;
-        this.age = age;
-        this.weight = weight;*/
     }
 
-    public Woodpecker( String name, int age, double weight,  double luck) {
+    public Woodpecker( String name, int age, double weight, double luck) {
         super("ƒ€тел", name, age, weight, true);
         System.out.println("запущен конструктор Woodpecker с параметрами и удачей");
         setLuck(luck);
@@ -30,12 +26,18 @@ public class Woodpecker extends Bird {
 
     @Override
     public void move() {
-
+        if (name == null) {
+            System.out.println(species + " взлетает");
+        }
+        else {System.out.println(name + " взлетает");}
     }
 
     @Override
     public void fromString(String str) {
-
+        Scanner scanner = new Scanner(str.replace(';',' '));
+        name = scanner.next();
+        age = scanner.nextInt();
+        weight = scanner.nextDouble();
     }
 
     public void knocks() {
@@ -63,9 +65,13 @@ public class Woodpecker extends Bird {
         this.luck = luck;
     }
 
-    //    ƒл€ теста:
-//    public static void main (String[] args) {
-//        Woodpecker woody = new Woodpecker("Woody",32,1.8);
-//        woody.knocks();
+//    public static void main(String[] args) {
+//        String str = "ƒ€тел; 5; 0,9";
+//        Woodpecker wp1 = new Woodpecker();
+//        wp1.fromString(str);
+//        wp1.luck = 0.15;
+//        wp1.knocks();
+//        wp1.move();
+//        System.out.println(wp1.name+"; "+wp1.age+"; "+wp1.weight);
 //    }
 }
